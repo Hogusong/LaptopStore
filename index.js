57741f5edc2158292abedc5661ccf7aed89742ae
“Young <hogusong1@gmail.com>
2019-06-29 11:31:46 -0400

    Read data and parse JSON data.

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+// Read data
+const jsonData = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const laptopData = JSON.parse(jsonData);
+console.log(laptopData);
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-type': 'text/html' });
   res.end('First message to test comunication');
