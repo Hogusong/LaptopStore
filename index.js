b430e282415541153da3c804a402396ba4f0ff0c
“Young <hogusong1@gmail.com>
2019-06-29 12:30:10 -0400

    Parse url and get pathName and query.id.

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ const laptopData = JSON.parse(jsonData);
 console.log(laptopData);
 
 const server = http.createServer((req, res) => {
+  // Parse url and get path to open HTML
+  const pathName = url.parse(req.url, true).pathname;
+  // Parse url and get id from query
+  const id = url.parse(req.url, true).query.id;
+
   res.writeHead(200, { 'Content-type': 'text/html' });
-  res.end('First message to test comunication');
+  res.end('First message to test comunication.' + '  path = ' + pathName + ',  id = ' + id);
 
 });
 
